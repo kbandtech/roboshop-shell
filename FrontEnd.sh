6fe45e1b3462dbea7abe84001ccfa7d9961448df
EC2 Default User <ec2-user@ip-172-31-15-61.ec2.internal>
2023-10-30 22:49:08 +0000

    roboshop files added

---
+++ FrontEnd.sh
@@ -0,0 +1,7 @@
+dnf install nginx -y
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+systemctl enable nginx 
+systemctl start nginx
