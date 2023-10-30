944d99f019375fd10a617d72056fffec16387bbd
kbandtech <138075144+kbandtech@users.noreply.github.com>
2023-10-31 10:28:10 +1100

    Update FrontEnd.sh
    commit changes

--- FrontEnd.sh
+++ FrontEnd.sh
@@ -1,7 +1,1 @@
-dnf install nginx -y
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
-systemctl enable nginx 
-systemctl start nginx
+
