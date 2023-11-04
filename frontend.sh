d99d7802a1fed0d9dd586f06b652ded0ce7bf6ef
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-04 05:40:03 +0000

    102

--- frontend.sh
+++ frontend.sh
@@ -1,30 +1,31 @@
 code_dir=$(pwd)
-
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
 
 echo -e "\e[35mInstalling nginx\e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>${log_file}
 
 echo -e "\e[35mRemoving old Content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 
 echo -e "\e[35mDownload FrontEnd Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
 echo -e "\e[35mExtracting Downloaded FrontEnd\e[0m"
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>${log_file}
+unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\e[35mCopying Nginx Config for Roboshop\e[0m"
-pwd
-ls -l
+echo -e "\e[35mCopying Nginx Config for Roboshop\e[0m" &>>${log_file}
+pwd &>>${log_file}
+ls -l &>>${log_file}
 #cp Configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf#
-cp ${code_dir}/Configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/Configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 echo -e "\e[35mEnabling nginx\e[0m"
-systemctl enable nginx 
+systemctl enable nginx &>>${log_file}
 
 echo -e "\e[35mStarting nginx\e[0m"
-systemctl restart nginx
+systemctl restart nginx &>>${log_file}
 
 ## Roboshop Config is not copied
 ## If any command is errored or failed, we need to stop the script
