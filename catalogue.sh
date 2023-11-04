67a77d0a7a4a34cfd42d9b4c45d4fce93935a617
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-04 09:19:49 +0000

    114

--- catalogue.sh
+++ catalogue.sh
@@ -27,7 +27,7 @@ print_head "Installing NodeJS Dependencies"
 npm install &>>${log_file}
 
 print_head "Copy SystemD Service file"
-cp Configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+cp ${code_dir}/Configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
 print_head "Reload SystemD"
 systemctl daemon-reload &>>${log_file}
@@ -39,7 +39,7 @@ print_head "Start Catalogue Service"
 systemctl restart catalogue &>>${log_file}
 
 print_head "Copy MongoDB Repo File"
-cp Configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+cp ${code_dir}/Configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "Install Mongo Client"
 dnf install mongodb-org-shell -y &>>${log_file}
