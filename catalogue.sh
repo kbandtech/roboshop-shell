6ba0811ab9aba1aee791eb3d1ac541e942c9160e
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-04 09:07:03 +0000

    112

--- catalogue.sh
+++ catalogue.sh
@@ -1,22 +1,48 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+print_head "Configure NodeJS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head "Install NodeJS"
+dnf install nodejs -y &>>${log_file}
+
+print_head "Create Roboshop User"
+useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory"
+mkdir /app &>>${log_file}
+
+print_head "Delete Old Content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading App Content"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app 
-unzip /tmp/catalogue.zip
-cd /app 
-npm install
-cp Configs/catalogue.service /etc/systemd/system/catalogue.service
 
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
+print_head "Extracting App Content"
+unzip /tmp/catalogue.zip &>>${log_file}
+cd /app
 
-cp Configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-org-shell -y
+print_head "Installing NodeJS Dependencies"
+npm install &>>${log_file}
 
-mongo --host mongodb.devopsb71.icu </app/schema/catalogue.js
+print_head "Copy SystemD Service file"
+cp Configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable Catalogue Service"
+systemctl enable catalogue &>>${log_file}
+
+print_head "Start Catalogue Service"
+systemctl restart catalogue &>>${log_file}
+
+print_head "Copy MongoDB Repo File"
+cp Configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+
+print_head "Install Mongo Client"
+dnf install mongodb-org-shell -y &>>${log_file}
+
+print_head "Load Schema"
+mongo --host mongodb.devopsb71.icu </app/schema/catalogue.js &>>${log_file}
