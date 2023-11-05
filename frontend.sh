9e4f921615b42cd52e5835f5543765cd96c3d5c3
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-05 01:11:21 +0000

    1113

--- frontend.sh
+++ frontend.sh
@@ -2,35 +2,64 @@ source common.sh
 
 print_head "Installing nginx"
 dnf install nginx -y &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo SUCCESS
+ else
+ echo FAILURE
+ fi
+ 
 
 print_head "Removing old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo SUCCESS
+ else
+ echo FAILURE
+ fi
 
 print_head "Download FrontEnd Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo SUCCESS
+ else
+ echo FAILURE
+ fi
 
 print_head "Extracting Downloaded FrontEnd" 
 cd /usr/share/nginx/html &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo SUCCESS
+ else
+ echo FAILURE
+ fi
 
 print_head "Copying Nginx Config for Roboshop" 
 pwd &>>${log_file}
 ls -l &>>${log_file}
 #cp Configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf#
 cp ${code_dir}/Configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo SUCCESS
+ else
+ echo FAILURE
+ fi
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo SUCCESS
+ else
+ echo FAILURE
+ fi
 
 print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo SUCCESS
+ else
+ echo FAILURE
+ fi
 
 
 
