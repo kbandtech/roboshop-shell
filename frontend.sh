483493471552f7221352b216495b4b78cf153717
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-05 00:01:58 +0000

    119

--- frontend.sh
+++ frontend.sh
@@ -2,28 +2,37 @@ source common.sh
 
 print_head "Installing nginx"
 dnf install nginx -y &>>${log_file}
+echo $?
 
 print_head "Removing old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+echo $?
 
 print_head "Download FrontEnd Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+echo $?
 
 print_head "Extracting Downloaded FrontEnd" 
 cd /usr/share/nginx/html &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
+echo $?
 
 print_head "Copying Nginx Config for Roboshop" 
 pwd &>>${log_file}
 ls -l &>>${log_file}
 #cp Configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf#
 cp ${code_dir}/Configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+echo $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
+echo $?
 
 print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
+echo $?
+
+
 
 ## Roboshop Config is not copied
 ## If any command is errored or failed, we need to stop the script
