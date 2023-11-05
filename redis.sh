50f80e8b72b7f19d47311599216831346cbbdb46
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-05 04:51:01 +0000

    user2

--- redis.sh
+++ redis.sh
@@ -13,15 +13,15 @@ dnf install redis -y  &>>${log_file}
 status_check $?
 
 print_head "Update Redis Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf&>>${log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>>${log_file}
 status_check $?
 
 print_head "Enable Redis"
-systemctl enable redis 
+systemctl enable redis &>>${log_file}
 status_check $?
 
 print_head "Start Redis"
-systemctl restart redis 
+systemctl restart redis &>>${log_file}
 status_check $?
 
 
