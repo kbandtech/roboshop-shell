a01db4b7afa195420334a826be36e94bb954b44c
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-05 03:48:38 +0000

    catalogue2

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,10 @@ dnf install nodejs -y &>>${log_file}
 status_check $?
 
 print_head "Create Roboshop User"
-useradd roboshop &>>${log_file}
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+ useradd roboshop &>>${log_file}
+fi 
 status_check $?
 
 print_head "Create Application Directory"
