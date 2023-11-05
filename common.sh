a76b808eaa2497425e74c436a68190b4b8dc94df
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-05 08:19:45 +0000

    latest

--- common.sh
+++ common.sh
@@ -17,3 +17,74 @@ status_check() {
      
     fi
 }
+
+NODEJS() {
+   
+    print_head "Configure NodeJS Repo"
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+    status_check $?
+    
+    print_head "Install NodeJS"
+    dnf install nodejs -y &>>${log_file}
+    status_check $?
+    
+    print_head "Create Roboshop ${component}"
+    id roboshop &>>${log_file}
+    if [ $? -ne 0 ]; then
+     ${component}add roboshop &>>${log_file}
+    fi 
+    status_check $?
+    
+    print_head "Create Application Directory"
+    if [ ! -d /app ]; then
+     mkdir /app &>>${log_file}
+    fi 
+    status_check $?
+    
+    print_head "Delete Old Content"
+    rm -rf /app/* &>>${log_file}
+    status_check $?
+    
+    print_head "Downloading App Content"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+    cd /app 
+    status_check $?
+    
+    print_head "Extracting App Content"
+    unzip /tmp/${component}.zip &>>${log_file}
+    cd /app
+    status_check $?
+    
+    print_head "Installing NodeJS Dependencies"
+    npm install &>>${log_file}
+    status_check $?
+    
+    print_head "Copy SystemD Service file"
+    cp ${code_dir}/Configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+    status_check $?
+    
+    print_head "Reload SystemD"
+    systemctl daemon-reload &>>${log_file}
+    status_check $?
+    
+    print_head "Enable ${component} Service"
+    systemctl enable ${component} &>>${log_file}
+    status_check $?
+    
+    print_head "Start ${component} Service"
+    systemctl restart ${component} &>>${log_file}
+    status_check $?
+    
+    print_head "Copy MongoDB Repo File"
+    cp ${code_dir}/Configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+    status_check $?
+    
+    print_head "Install Mongo Client"
+    dnf install mongodb-org-shell -y &>>${log_file}
+    status_check $?
+    
+    print_head "Load Schema"
+    mongo --host mongodb.devopsb71.icu </app/schema/${component}.js &>>${log_file}
+    status_check $?
+
+}
