1274e0e9b758badcc5d2a26c4c3785c1848b4c07
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-05 03:26:08 +0000

    catalogue

--- mongodb.sh
+++ mongodb.sh
@@ -2,18 +2,23 @@ source common.sh
 
 print_head "Setup MongoDB repository"
 cp ${code_dir}/Configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
 
 print_head "Install MongoDB"
 dnf install mongodb-org -y &>>${log_file}
+status_check $?
 
 print_head "Update MongoDB Listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
+status_check $?
 
 print_head "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
+status_check $?
 
 print_head "Start MongoDB Service"
 systemctl restart mongod &>>${log_file}
+status_check $?
 
 
 #update /etc/mongod.conf file from 127.0.0.0 with 0.0.0.0
