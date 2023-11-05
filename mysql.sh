78cf4ada977983e60a00fe9233d86f518ac4e6b3
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-05 10:33:29 +0000

    mysql

---
+++ mysql.sh
@@ -0,0 +1,33 @@
+source common.sh
+
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+ echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
+ exit 1
+ 
+fi
+
+
+print_head "Disabling MYSQL 8 Version"
+dnf module disable mysql -y
+status_check $?
+
+print_head ""
+dnf install mysql-community-server -y
+status_check $?
+
+print_head "Enable MYSQL Service"
+systemctl enable mysqld
+status_check $?
+
+print_head "Start MySQL Service"
+systemctl start mysqld
+status_check $?
+
+print_head "Set password"
+mysql_secure_installation --set-root-pass ${mysql_root_password}
+status_check $?
+
+print_head ""
+mysql -uroot -pRoboShop@1
+status_check $?
