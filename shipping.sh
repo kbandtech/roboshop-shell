9c3e118d3f617c13818d492054edae9a0a96b7c1
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-09 22:18:59 +0000

    new updates1

--- shipping.sh
+++ shipping.sh
@@ -2,7 +2,7 @@ source common.sh
 
 mysql_root_password=$1
 
-if [-z "${mysql_root_password}" ]; then
+if [-Z "${mysql_root_password}" ]; then
  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
  exit 1
 
