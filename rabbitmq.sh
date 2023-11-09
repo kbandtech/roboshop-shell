e65a20e5c348332a22640be62c1326a85ea6a5f6
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-09 02:05:00 +0000

    added RabbitMQ1

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,7 +2,7 @@ source common.sh
 
 roboshop_app_password=$1
 if [ -z "${roboshop_app_password}" ]; then
- echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
+ echo -e "\e[31mMissing RabbitMQ App User Password argument\e[0m"
  exit 1
  
 fi
